ab77d6070ded4a3c496402d717b1c881f66a9ae1
marlemus2022 <112045787+marlemus2022@users.noreply.github.com>
2023-06-06 16:24:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -469,8 +469,8 @@ c14.image(imageROC, caption="Rociadores. Tomado de: https://media.istockphoto.co
 
 c15.markdown("<h5 style='text-align: center; color: #930000;'>Porcentaje de incendios en los que funcionaron efectivamente  las alarmas de humo</h5>", unsafe_allow_html=True)
 
-st.write(AH)
-st.write(AH.columns)
+#st.write(AH)
+#st.write(AH.columns)
 #conteo_eventos = AH['performance_of_system'].value_counts()
 AH=AH.rename(columns={'Performance of smoke alarm device, residential fires':'performance_of_system'})
 conteo_performance = AH['performance_of_system'].value_counts()
@@ -496,7 +496,9 @@ figah.update_layout(template = 'simple_white',
 c15.plotly_chart(figah)
 
 ###
-c15.markdown("<h6 style='text-align: center; color: #525252;'>El porcentaje o tasa de efectividad  de funcionamiento de las alarmas de humo es del 20%.</h6>", unsafe_allow_html=True)
+c15.markdown("<h6 style='text-align: left; color: #525252;'>El porcentaje o tasa de efectividad  de funcionamiento de las alarmas de humo es del 20%.</h6>", unsafe_allow_html=True)
 
 imageAH= "alarmahumo.jpg"
 c15.image(imageAH, caption="Alarma de humo. Tomado de: https://media.istockphoto.com/id/1332514392/es/foto/detector-de-humo-y-alarma-contra-incendios-en-el-fondo-de-acci%C3%B3n.jpg?s=612x612&w=0&k=20&c=PRkBl_EFCHHO0LArDQE4cDs6CkCm_saix-YW9yyfmtE=", width=500, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+
+st.markdown("<h1 style='text-align: center; color: #525252;'>MUCHAS GRACIAS</h1>", unsafe_allow_html=True)
